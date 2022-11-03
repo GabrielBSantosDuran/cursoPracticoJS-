7332edf6eb05de8f0c3f3a42355d49cd07429b3b
MrFrost14 <gabriel_bodomir@hotmail.com>
2022-11-03 16:46:14 -0400

    adicion de carrito y implementacion de mejoras frontend

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
-const menuEmail= document.querySelector('.navbar-email');
-const desktopMenu= document.querySelector('.desktop-menu');
-const menuHamIcon= document.querySelector('.menu');
-const mobileMenu= document.querySelector('.mobile-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobilepMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobilepMenu);
+menuCarrito.addEventListener("click", toggleCarritoAside);
 
-function toggleDesktopMenu(){
-    console.log('click')
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {
+  console.log("click");
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobilepMenu(){
-    console.log('click mobile menu');
-    mobileMenu.classList.toggle('inactive');
+function toggleMobilepMenu() {
+  console.log("click mobile menu");
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClose = mobileMenu.classList.contains("inactive");
+  // console.log("click carrito aside");
+
+  if (!isMobileMenuClose) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
